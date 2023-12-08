b4a557add6ae5097fe9c811c48de78c2d76513d8
ymd-ayk <aykenv.182@gmail.com>
2023-12-08 15:07:45 +0900

    add: フラッシュメッセージ、エラーメッセージ追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_background_color(type)
+    case type.to_sym
+    when :success then "bg-green-100 border-green-400 text-green-700"
+    when :notice  then "bg-blue-100 border-blue-400 text-blue-700"
+    when :alert   then "bg-red-100 border-red-400 text-red-700"
+    when :error   then "bg-yellow-100 border-yellow-400 text-yellow-700"
+    else "bg-gray-100 border-gray-400 text-gray-700"
+    end
+  end
 end
