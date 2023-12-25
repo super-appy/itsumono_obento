a5a95c2fda1b3d7bfa2d0301cf73736e6abef89e
ymd-ayk <aykenv.182@gmail.com>
2023-12-25 11:38:13 +0900

    add: お弁当ログのいいね機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_070626) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_25_004447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_070626) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["lunchbox_log_id"], name: "index_like_lunchbox_logs_on_lunchbox_log_id"
+    t.index ["user_id", "lunchbox_log_id"], name: "index_like_lunchbox_logs_on_user_id_and_lunchbox_log_id", unique: true
     t.index ["user_id"], name: "index_like_lunchbox_logs_on_user_id"
   end
 
