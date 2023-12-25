72279d80a60b7ba6e8068fb6acda92e75a242f4a
ymd-ayk <aykenv.182@gmail.com>
2023-12-25 11:53:48 +0900

    edit: ボタンのajax化

--- app/controllers/like_lunchbox_logs_controller.rb
+++ app/controllers/like_lunchbox_logs_controller.rb
@@ -2,12 +2,10 @@ class LikeLunchboxLogsController < ApplicationController
   def create
     lunchbox_log = LunchboxLog.find(params[:lunchbox_log_id])
     current_user.like(lunchbox_log)
-    redirect_to lunchbox_logs_path, success: 'お気に入りできたよ'
   end
 
   def destroy
     lunchbox_log = current_user.like_lunchbox_logs.find(params[:id]).lunchbox_log
     current_user.unlike(lunchbox_log)
-    redirect_to lunchbox_logs_path, success: 'お気に入り外したよ'
   end
 end
