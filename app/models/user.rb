e9bd3a6aa2d833903c40fd066c51b241e52974cd
ymd-ayk <aykenv.182@gmail.com>
2024-01-08 17:43:41 +0900

    edit: プロフィールにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,6 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  # avatorはpngかjpeg,jpgのみ
 
   mount_uploader :avator, AvatorUploader
 end
