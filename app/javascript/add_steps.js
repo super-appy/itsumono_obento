153b3abd83aa419edab3adeb54574909f43733b3
ymd-ayk <aykenv.182@gmail.com>
2024-01-13 08:36:51 +0900

    edit: 不要な処理を削除

--- app/javascript/add_steps.js
+++ app/javascript/add_steps.js
@@ -15,7 +15,6 @@ function updateSteps() {
     // 仮の値を設定する例
   document.querySelectorAll('.step-index').forEach((element, index) => {
     // ここで設定する値は、実際のアプリケーションロジックに基づく必要があります。
-    console.log('dekiteru?')
     element.value = index + 1; // 例えば、インデックスに1を加えた値を設定
   });
 }
