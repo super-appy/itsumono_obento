5f73db55eb58df85f0d69f37f1ff8a02cfa0101e
ymd-ayk <aykenv.182@gmail.com>
2024-01-12 15:12:04 +0900

    add: レシピ生成中にモーダルを表示

--- app/javascript/flash_message.js
+++ app/javascript/flash_message.js
@@ -1,14 +1,14 @@
-// これだと動かないのでキャンセルボタンの実装は見送り?
+// // これだと動かないのでキャンセルボタンの実装は見送り?
 
-document.addEventListener('DOMContentLoaded', () => {
-  // 全てのキャンセルボタンを選択　※ここの要素が取れてないことが発覚！！！
-  const cancelButtons = document.querySelectorAll('#cancel_button');
-  console.log(cancelButtons, 'button')
-  // 各キャンセルボタンにイベントリスナーを設定
-  cancelButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      // クリックされたボタンの親要素であるフラッシュメッセージを非表示にする
-      button.closest('.flash_message').style.display = 'none';
-    });
-  });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   // 全てのキャンセルボタンを選択　※ここの要素が取れてないことが発覚！！！
+//   const cancelButtons = document.querySelectorAll('#cancel_button');
+//   console.log(cancelButtons, 'button')
+//   // 各キャンセルボタンにイベントリスナーを設定
+//   cancelButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//       // クリックされたボタンの親要素であるフラッシュメッセージを非表示にする
+//       button.closest('.flash_message').style.display = 'none';
+//     });
+//   });
+// });
--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -1,30 +1,16 @@
-(function () {
-  // モーダルボタンとモーダルエリアの全要素を取得
-  const modalButtons = document.querySelectorAll('[id^="openModal"]');
-  const modalAreas = document.querySelectorAll('.modalArea');
-  const modalBgs = document.querySelectorAll('[id^="modalBg"]');
+document.addEventListener("turbo:load", () => {
+  setupModal();
+});
 
-  // 各ボタンにイベントリスナーを設定
-  modalButtons.forEach(button => {
-    button.addEventListener('click', function () {
-      const index = this.id.replace('openModal', '');
-      const modal = document.querySelector(`#modalArea${index}`);
-      showModal(modal);
-    });
-  });
+function setupModal(){
+  const submitButton = document.getElementById('openModal');
+  const modalArea = document.getElementById('modalArea');
 
-  // 閉じるボタンと背景クリックのイベントも設定
-  modalAreas.forEach(area => {
-    const closeModal = area.querySelector('.closeModal');
-    closeModal.addEventListener('click', () => hideModal(area));
+  // フォーム送信時にモーダルを表示
+  submitButton.addEventListener('click', function (e) {
+    showModal(modalArea);
   });
-
-  modalBgs.forEach(bg => {
-    bg.addEventListener('click', () => {
-      const modal = bg.closest('.modalArea');
-      hideModal(modal);
-    });
-  });
+  }
 
   function showModal(modal) {
     // モーダル表示の処理
@@ -32,12 +18,13 @@
     setTimeout(() => modal.classList.remove('opacity-0'), 10);
   }
 
-  function hideModal(modal) {
+  // モーダル非表示の関数は、後でRailsのコントローラから呼び出すためにグローバルに公開
+  window.hideModal = function (modal) {
     // モーダル非表示の処理
     modal.classList.add('opacity-0');
     modal.addEventListener('transitionend', function handler() {
       modal.classList.add('hidden');
       modal.removeEventListener('transitionend', handler);
     });
-  }
-}());
+  };
+
