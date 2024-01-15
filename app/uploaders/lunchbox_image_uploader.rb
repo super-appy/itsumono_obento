f8f430356111004bace96bc114aab764360437db
ymd-ayk <aykenv.182@gmail.com>
2024-01-15 15:21:56 +0900

    #118 edit: お弁当のデフォルト画像を変更

--- app/uploaders/lunchbox_image_uploader.rb
+++ app/uploaders/lunchbox_image_uploader.rb
@@ -20,7 +20,7 @@ class LunchboxImageUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    'icon_default.png'
+    'default.png'
   end
 
   # Process files as they are uploaded:
