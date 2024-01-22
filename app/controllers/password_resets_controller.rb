a6fbcd87a60c0a28e364b8e3a1becd157454ad2a
ymd-ayk <aykenv.182@gmail.com>
2024-01-22 12:24:10 +0900

    add: パスワードリセット機能追加

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,38 @@
+class PasswordResetsController < ApplicationController
+  skip_before_action :require_login
+
+  def new; end
+
+  def create
+    @user = User.find_by_email(params[:email])
+    @user.deliver_reset_password_instructions! if @user
+    redirect_to root_path, notice: 'パスワードリセットのメールを送信しました'
+  end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+      return
+    end
+  end
+
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+      return
+    end
+
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, success: 'パスワードがリセットされました'
+    else
+      render :action => "edit"
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :reset_password_token, presence: true, uniqueness: true, allow_nil: true
 
   mount_uploader :avator, AvatorUploader
 end
