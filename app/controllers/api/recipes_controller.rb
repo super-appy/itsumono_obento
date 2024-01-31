e86ad519d9d65d7080a44721479fda11b067ab0e
ymd-ayk <aykenv.182@gmail.com>
2024-02-01 08:13:46 +0900

    edit: ユーザーが1日1回しか生成できないよう修正

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -2,8 +2,7 @@ module Api
   class RecipesController < ApplicationController
     include SelectListable
     before_action :set_select_lists, only: :new
-    skip_before_action :require_login, only: [:new, :create]
-
+    before_action :require_login, only: [:new, :create]
 
     def new
       if can_create_recipe?
@@ -14,11 +13,7 @@ module Api
     end
 
     def create
-      if logged_in?
-        @recipe = current_user.recipes.build(recipe_params)
-      else
-        @recipe = Recipe.new(recipe_params)
-      end
+      @recipe = current_user.recipes.build(recipe_params)
       @recipe.controller_name = 'api/recipes'
 
       if @recipe.valid?
@@ -29,7 +24,6 @@ module Api
 
           if @recipe.save
             session[:from_action] = 'create'
-            set_api_request_cookie
             respond_to do |format|
               format.js { render js: "window.hideModal(document.getElementById('modalArea'));" }
               format.html { redirect_to recipe_path(@recipe), notice: 'レシピが生成されました。' }
@@ -56,34 +50,12 @@ module Api
     private
 
     def can_create_recipe?
-      unless logged_in?
-        return api_request_allowed?
-      end
-
       !current_user.recipes.where(created_at: Date.today.beginning_of_day..Date.today.end_of_day)
                       .where.not(api_resp: [nil, '']).exists?
     end
 
     def recipe_params
-      if logged_in?
-        params.require(:recipe).permit(:time_required, :taste, tag_ids:[]).merge(user_id: current_user.id)
-      else
-        params.require(:recipe).permit(:time_required, :taste, tag_ids:[])
-      end
-    end
-
-    def api_request_allowed?
-      last_request_date = cookies[:last_api_request_date]
-      last_request_date.blank? || Date.parse(last_request_date) < Date.today
-    end
-
-    def set_api_request_cookie
-      cookies[:last_api_request_date] = {
-        value: Date.today.to_s,
-        expires: Date.today.end_of_day,
-        secure: Rails.env.production?,
-        httponly: true
-      }
+      params.require(:recipe).permit(:time_required, :taste, tag_ids:[]).merge(user_id: current_user.id)
     end
   end
 end
